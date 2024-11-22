cb5ef7d215c958f2cf6b894c603fdbf00c1f7154
fathyafathazz <fathyafathazzra@gmail.com>
2024-11-22 11:33:10 +0700

    coba

--- model/category.go
+++ model/category.go
@@ -1,6 +1,10 @@
 package model
 
+import "time"
+
 type Category struct {
     ID   int    `json:"id"`
     Name string `json:"name"`
+    CreatedAt time.Time `json:"createdAt"` 
+    UpdatedAt time.Time `json:"updatedAt"`
 }
