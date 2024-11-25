72f3d9cefbff37d312d02377bdd8779156a7a79e
fathyafathazz <fathyafathazzra@gmail.com>
2024-11-25 11:38:55 +0700

    remove key

--- config/api.go
+++ config/api.go
@@ -1,7 +1,5 @@
 package config
 
-import "os"
-
 var WAAPIQRLogin string = "https://api.wa.my.id/api/whatsauth/request"
 
 var WAAPIMessage string = "https://api.wa.my.id/api/v2/send/message/text"
@@ -21,9 +19,3 @@ var WAAPIGetDevice string = "https://api.wa.my.id/api/device/"
 var PublicKeyWhatsAuth string
 
 var WAAPIToken string
-
-var APIGETPDLMS string = "https://pamongdesa.kemendagri.go.id/webservice/public/user/get-by-phone?number="
-
-var APITOKENPD string = os.Getenv("PDTOKEN")
-
-var PUBLICKEY string = "0d6171e848ee9efe0eca37a10813d12ecc9930d6f9b11d7ea594cac48648f022"
