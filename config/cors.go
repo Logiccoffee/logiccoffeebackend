2e1a5306467ac5337718b75d0a302faf40e5507a
fathyafathazz <fathyafathazzra@gmail.com>
2024-11-25 11:02:36 +0700

    asli

--- config/cors.go
+++ config/cors.go
@@ -1,18 +1,14 @@
 package config
-
 import (
 	"net/http"
 )
-
 // Daftar origins yang diizinkan
 var Origins = []string{
 	"https://www.bukupedia.co.id",
 	"https://naskah.bukupedia.co.id",
 	"https://bukupedia.co.id",
-	"https://logiccoffee.id.biz.id", // Menambahkan domain frontend Anda
-	// "http://127.0.0.1:5500",  //menambahkan localhost
+	"https://logiccoffee.id.biz.id", //add origin logiccoffee
 }
-
 // Fungsi untuk memeriksa apakah origin diizinkan
 func isAllowedOrigin(origin string) bool {
 	for _, o := range Origins {
@@ -22,29 +18,24 @@ func isAllowedOrigin(origin string) bool {
 	}
 	return false
 }
-
 // Fungsi untuk mengatur header CORS
 func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 	origin := r.Header.Get("Origin")
-
-	// Jika origin tidak diizinkan, abaikan CORS dan tidak lanjutkan
-	if !isAllowedOrigin(origin) {
+	if isAllowedOrigin(origin) {
+		// Set CORS headers for the preflight request
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.WriteHeader(http.StatusNoContent)
+			return true
+		}
+		// Set CORS headers for the main request.
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		return false
 	}
-
-	// Set CORS headers untuk permintaan preflight (OPTIONS)
-	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
-		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		w.Header().Set("Access-Control-Max-Age", "3600")
-		w.WriteHeader(http.StatusNoContent) // Preflight request berhasil, tanpa body
-		return true
-	}
-
-	// Set CORS headers untuk permintaan utama (GET, POST, PUT, DELETE, dll)
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Origin", origin)
 	return false
 }
